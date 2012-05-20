843892efa86a8b6456f5612082827fb6f71249eb
ide user ide_whitegiraffe <yosuke.kondo@gmail.com>
2012-05-20 03:10:53 +0000

    using mongodb

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 var http = require('http');
+var mongo = require('mongoskin');
+var conf = require('./conf.js');
+
+conf.setupenv();
+var db = mongo.db(process.env.MONGO_URI);
+
 http.createServer(function(req, res){
     "use strict";
-    res.writeHead(200, {'Content-Type':'text/plain'});
-    res.end('Hello World!\n');
+
+    db.collection('things').find({greeting:/^Hello/},{greeting:true}).limit(10)
+    .toArray(function(err, items){
+        if(err) throw err;
+        
+        res.writeHead(200, {'Content-Type':'text/plain'});
+        res.end('Hello World!\n\n' + JSON.stringify(items));
+    });
+
 }).listen(process.env.PORT);
