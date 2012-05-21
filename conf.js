e6fdad8cf0ed790a790e27f0f0d25ef8768b0f60
ide user ide_whitegiraffe <yosuke.kondo@gmail.com>
2012-05-21 12:47:40 +0000

    removing conf.js

--- conf.js
+++
@@ -1,4 +0,0 @@
-exports.setupenv = function(){
-    process.env.MONGO_URI = (process.env.MONGO_URI || 'localhost:27017/test');
-//    process.env.MONGO_URI = (process.env.MONGO_URI || '<username>:<password>@<hostname>:<port>/<dbname>');
-};
